0b4f29f607d36579d57475cac428b92974a534dc
Barqawiz <barqawi.88@outlook.com>
2024-11-03 14:10:48 +0000

    edit values

--- sunfounder_component/sample_soil_moisture.py
+++ sunfounder_component/sample_soil_moisture.py
@@ -5,7 +5,7 @@ ADC.setup(0x48)  # Initialize PCF8591 at address 0x48
 
 try:
     while True:  # Continuously read and print moisture level
-        print(ADC.read(1))  # Read from Soil Moisture Sensor at AIN1
+        print(ADC.read(2))  # Read from Soil Moisture Sensor at AIN1
         time.sleep(0.2)  # Delay of 0.2 seconds
 except KeyboardInterrupt:
     print("Exit")  # Exit on CTRL+C
---
+++ sunfounder_component/touch_motor.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO control
+pump_channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pump_channel, GPIO.OUT)
+
+def start_motor(pin):
+    try:
+        # my pump+relay works with GPIO.LOW
+        print('- start motor')
+        GPIO.output(pin, GPIO.LOW)
+    except KeyboardInterrupt:
+        print('exception')
+        GPIO.cleanup()
+
+def stop_motor():
+    print('- stop motor')
+    GPIO.cleanup()
+
+
+
+stop_motor()
